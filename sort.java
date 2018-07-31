7ac7f42e467e41a9c30ffc7cb419e66bfc677cbe
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-07-31 12:59:50 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n,temp;
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	int a[]=new int[n];
+	for(int i=0;i<n;i++)
+	 {
+		a[i]=sc.nextInt();
+	 }
+	  for(int i=0;i<n;i++)
+	   {
+		 for(int j=i+1;j<n;j++)
+		 {
+			if(a[i]>a[j])
+			 {
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+		   	}
+		}
+	}
+	for(int i=0;i<=n;i++)
+	{
+		System.out.print(a[i]+" ");
+	}
+}
+}
